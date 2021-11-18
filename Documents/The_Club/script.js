4644850110bcb8180f85451b26a711c3f962a4ce
Cylin George <g.cylin1@gmail.com>
2021-11-17 21:01:50 -0500

    first commit

---
+++ Documents/The_Club/script.js
@@ -0,0 +1,10 @@
+//on click of burger menu nav options appear 
+let menu = document.querySelector('#burger'); 
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = ) => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+//
+
